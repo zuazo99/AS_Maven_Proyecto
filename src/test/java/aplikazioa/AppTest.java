f7cc2bde053d639e9e8c8ac6d5ec86a786dfed4a
gaizka <gzuazo63@gmail.com>
2021-12-07 21:02:40 +0100

    Pruebas implementadas y actualizacion del programa principal

--- src/test/java/aplikazioa/AppTest.java
+++ src/test/java/aplikazioa/AppTest.java
@@ -3,9 +3,68 @@ package aplikazioa;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class AppTest extends TestCase {
+    private static App app;
+
+
+    @BeforeAll
+    /*
+        El metodo que se marque con @BeforeAll se ejecutara
+        una unica vez antes que cualquier metodo de prueba
+     */
+    public static void init(){
+        app = new App();
+    }
+
+    @AfterAll
+    /*
+        Un metodo de limpieza que se ejecute solo una vez
+        despues de todos los metodos de test.
+     */
+    public static void finish(){
+        app = null;
+    }
+
+    @org.junit.Test
+    public void testSuma() {
+        assertEquals(4,App.suma(2,2));
+    }
+
+    @org.junit.Test
+    public void testSumaHamcrest() {
+       // assertThat(app, notNullValue());
+        assertThat(App.suma(2,3), is(5));
+        assertThat(App.suma(4,4), equalTo(8));
+    }
+
+
+    @org.junit.Test
+    public void testResta() {
+        assertEquals(2,App.resta(4,2));
+    }
+
+    @org.junit.Test
+    public void testMultiplicacion(){
+        assertEquals(9, App.multiplicacion(3,3));
+    }
+
+    @org.junit.Test
+    public void testMultiplicacionHamcrest(){
+        assertThat(App.multiplicacion(4,3), is(12));
+    }
+
+    @org.junit.Test
+    public void testDivision(){
+        assertEquals(3, App.division(9,3));
+    }
 
     public AppTest(String testName){
         super(testName);
@@ -18,5 +77,7 @@ public class AppTest extends TestCase {
     public void testApp(){
         assertTrue(true);
     }
+
+
 
 }
